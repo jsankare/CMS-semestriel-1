2fec934f872f4e2be2f7164fe6a482b397d9e079
Jordan <jordan@MacBook-Pro-de-Jordan.local>
2024-05-20 11:28:38 +0200

    user id is stored into supervariable and displayed when user logs in

--- www/Controllers/Security.php
+++ www/Controllers/Security.php
@@ -11,6 +11,9 @@ class Security{
     {
         //Je vérifie que l'utilisateur n'est pas connecté sinon j'affiche un message
 
+        // Debut de session
+        session_start();
+
         $form = new Form("Login");
         if( $form->isSubmitted() && $form->isValid() ) {
             // Met toutes les infos du user correspondant dans une variable
@@ -18,10 +21,13 @@ class Security{
             if ($user) {
                 // Compare le password saisi par l'user et celui correspondant au mail en DB
                 if (password_verify($_POST["password"], $user->getPassword())) {
+                    // on store le user ID dans la session
+                    $_SESSION['user_id'] = $user->getId();
+                    echo $_SESSION['user_id'];
                     echo "Login successful!";
                 }
             } else {
-                echo "User not found.";
+                echo "Invalid email or password";
             }
         }
         $view = new View("Security/login"); // instantiation
@@ -38,7 +44,7 @@ class Security{
         if( $form->isSubmitted() && $form->isValid() ) {
             $dbUser = (new User())->findOneByEmail($_POST["email"]);
             if ($dbUser) {
-                echo "User déjà register";
+                echo "Un user existe déjà avec cette adresse email";
                 exit;
             }
             $user = new User(); // Initialisation d'un nouveau User
@@ -56,6 +62,24 @@ class Security{
     }
     public function logout(): void
     {
+        // retire le user ID de la session pour logout
+        unset($_SESSION['userId']);
         echo "Se déconnecter";
+    }
+
+    public function profile(): void
+    {
+        // Start session
+        session_start();
+
+        // Check if user is logged in (user ID is stored in session)
+        if (isset($_SESSION['user_id'])) {
+            // User is logged in
+            echo "Welcome, you are logged in!";
+        } else {
+            // User is not logged in
+            echo "You need to log in to access this page.";
+            // You can redirect the user to the login page if needed
+        }
     }
 }
--- www/dump.sql
+++ www/dump.sql
@@ -20,6 +20,7 @@ SET TIME ZONE 'UTC';
 -- Structure de la table `esgi_user`
 --
 
+DROP TABLE IF EXISTS public.esgi_user;
 CREATE TABLE public.esgi_user (
                                   id SERIAL PRIMARY KEY,
                                   firstname VARCHAR(50) NOT NULL,
@@ -27,6 +28,15 @@ CREATE TABLE public.esgi_user (
                                   email VARCHAR(320) NOT NULL,
                                   password VARCHAR(255) NOT NULL,
                                   status SMALLINT NOT NULL,
+                                  date_inserted TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                                  date_updated TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+DROP TABLE IF EXISTS public.esgi_page;
+CREATE TABLE public.esgi_page (
+                                  id SERIAL PRIMARY KEY,
+                                  title VARCHAR(50) NOT NULL,
+                                  content VARCHAR(255) NOT NULL,
                                   date_inserted TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
                                   date_updated TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
