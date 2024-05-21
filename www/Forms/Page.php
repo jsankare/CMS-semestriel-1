3f5c82582ef68f6b71b9cc04e4de5120429616ba
Brutal_Mac <jordan.sankare@gmail.com>
2024-05-21 15:54:30 +0200

    created page form

---
+++ www/Forms/Page.php
@@ -0,0 +1,33 @@
+<?php
+namespace App\Forms;
+class Page
+{
+
+    public static function getConfig(): array
+    {
+        return [
+            "config"=>[
+                "action"=>"",
+                "method"=>"POST",
+                "submit"=>"Ajouter"
+            ],
+            "inputs"=>[
+                "title"=>[
+                    "type"=>"text",
+                    "min"=>2,
+                    "max"=>50,
+                    "placeholder"=>"Titre de la page",
+                    "required"=>true,
+                    "error"=>"Le titre de la page doit faire entre 2 et 50 caractères"
+                ],
+                "description"=>[
+                    "type"=>"text-area",
+                    "placeholder"=>"Entrez ici une description de la page",
+                    "required"=>true,
+                    "error"=>"La description de la page ne peut pas faire plus de 255 caractères"
+                ]
+            ]
+
+        ];
+    }
+}
