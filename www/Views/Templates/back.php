b19bec330ff51df8a4e013dc6e221e90c8b9616b
Brutal_Mac <jordan.sankare@gmail.com>
2024-05-24 15:28:30 +0200

    nav is active for all back templates, included a dashboard

--- www/Views/Templates/back.php
+++ www/Views/Templates/back.php
@@ -4,12 +4,31 @@
         <meta charset="UTF-8">
         <title>Ceci est mon back</title>
         <meta name="description" content="Super site avec une magnifique intégration">
-        <link rel="stylesheet" href="/css/main.css">
+        <link rel="stylesheet" href="/css/back.css">
     </head>
     <body>
-        <h1>Template Back - CMS</h1>
-        <h2>Working on nav</h2>
-        <!-- intégration de la vue -->
-        <?php include "../Views/".$this->view.".php";?>
+        <main class="mainBack" >
+            <aside class="navbar" >
+                <a href="/dashboard">dashboard</a>
+                <h3>Barre de navigation</h3>
+                <section>
+                    <h4>Pages</h4>
+                    <ul>
+                        <li><a href="/page/create">Créer une page</a></li>
+                    </ul>
+                </section>
+                <section>
+                    <h4>Articles</h4>
+                    <ul>
+                        <li><a href="/article/create">Créer un article</a></li>
+                    </ul>
+                </section>
+            </aside>
+            <section class="content" >
+                <h1>Template Back - CMS</h1>
+                <!-- intégration de la vue -->
+                <?php include "../Views/".$this->view.".php";?>
+            </section>
+        </main>
     </body>
 </html>
