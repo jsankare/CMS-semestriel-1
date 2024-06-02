006ed8c2d2568e3cb2627374d49096ffc9cbe305
pierre265 <kevinngako@gmail.com>
2024-06-02 22:02:16 +0200

    Dumps de table comment

--- www/dump.sql
+++ www/dump.sql
@@ -57,17 +57,27 @@ CREATE TABLE public.esgi_article (
     date_updated TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
+
 DROP TABLE IF EXISTS public.esgi_comments CASCADE;
+
+
+DROP TYPE IF EXISTS comment_status CASCADE;
+
+-- Créer le type enum
+CREATE TYPE comment_status AS ENUM ('pending', 'approved', 'rejected');
+
+
 CREATE TABLE esgi_comments (
     id SERIAL PRIMARY KEY,
     article_id INT NOT NULL,
     user_id INT NOT NULL,
     content TEXT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    status ENUM('pending', 'approved', 'rejected') DEFAULT 'pending',
+    status comment_status DEFAULT 'pending',
     FOREIGN KEY (article_id) REFERENCES articles(id),
     FOREIGN KEY (user_id) REFERENCES users(id)
-)
+);
+
 
 -- On supprime les lignes concernant les index et AUTO_INCREMENT car déjà gérés par SERIAL
 
