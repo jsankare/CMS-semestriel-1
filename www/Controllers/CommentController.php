fe81a59ecf4b1b30ac6faa7ed275e52c9d7a078d
pierre265 <kevinngako@gmail.com>
2024-06-02 22:06:10 +0200

    CommentController multiple changes

--- www/Controllers/CommentController.php
+++ www/Controllers/CommentController.php
@@ -12,56 +12,25 @@ class CommentController
     public function add(): void
     {
         $commentForm = new Form("Comment");
+        $article_id = $_GET['article_id'];
 
         if ($commentForm->isSubmitted() && $commentForm->isValid()) {
             $comment = new Comment();
-            $comment->setArticleId($_POST["article_id"]);
-            $comment->setUserId($_SESSION['user_id']);
+            $comment->setArticleId($article_id);
+            $comment->setUserId($_SESSION['user_id']); 
             $comment->setContent($_POST["content"]);
             $comment->setStatus('pending');
             $comment->save();
 
-            header('Location: /article/view?id=' . $_POST["article_id"]);
+            header('Location: /article/home');
             exit();
-        }
-
-        $view = new View("Comment/add", "front");
-        $view->assign('commentForm', $commentForm->build());
-        $view->assign('error', 'Le formulaire n\'a pas été soumis ou n\'est pas valide.');
-        $view->render();
-    }
-
-    public function moderate(): void
-    {
-        $comments = (new Comment())->findAll();
-
-        if ($comments) {
-            $view = new View("Comment/moderate", "back");
-            $view->assign('comments', $comments);
+        } else {
+            $view = new View("Comment/create", "back");
+            $view->assign('commentForm', $commentForm->build());
+            $view->assign('article_id', $article_id);
+            $view->assign('error', 'Le formulaire n\'a pas été soumis ou n\'est pas valide.');
             $view->render();
-        } else {
-            echo 'Aucun commentaire à modérer.';
         }
-    }
-
-    public function approve(): void
-    {
-        if (isset($_GET['id'])) {
-            $comment = (new Comment())->findOneById($_GET['id']);
-            if ($comment) {
-                $comment->setStatus('approved');
-                $comment->save();
-            } else {
-                echo 'Commentaire non trouvé.';
-                exit();
-            }
-        } else {
-            echo 'ID de commentaire non spécifié.';
-            exit();
-        }
-
-        header('Location: /comment/moderate');
-        exit();
     }
 
     public function reject(): void
@@ -81,6 +50,15 @@ class CommentController
 
         header('Location: /comment/moderate');
         exit();
+    }
+
+    public function list(): void
+    {
+        $articleModel = new Comment();
+        $comments = $articleModel->findAll();
+        $view = new View("Comment/home", "back");
+        $view->assign('comments', $comments);
+        $view->render();
     }
 
     
