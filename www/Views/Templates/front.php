05237a128c72102d92bfa407bda4247d1d4b2f8a
pierre265 <kevinngako@gmail.com>
2024-06-04 04:43:14 +0200

    Rebase et gestion des erreurs

--- www/Views/Templates/front.php
+++ www/Views/Templates/front.php
@@ -5,8 +5,24 @@
         <title>Ceci est mon front</title>
         <meta name="description" content="Super site avec une magnifique intégration">
         <link rel="stylesheet" href="/css/main.css">
+        <link rel="stylesheet" href="/css/front.css">
+
     </head>
     <body>
+            <!-- Navbar -->
+    <div class="navbar">
+        <a href="#">Accueil</a>
+        <a href="#">Profil</a>
+        
+        <?php
+        if (isset($pages) && !empty($pages)) {
+            foreach ($pages as $page) {
+                echo "<a href='/page/show?id={$page->getId()}'>{$page->getTitle()}</a>";
+            }
+        }
+        ?>
+        <a href="/security/logout">Déconnexion</a>
+    </div>
         <h1>Template Front - CMS</h1>
         <!-- intégration de la vue -->
         <?php include "../Views/".$this->view.".php";?>
