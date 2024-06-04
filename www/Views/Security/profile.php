9cc6d7d4e4d30cc42cf5d9edf16e19775c9385e6
pierre265 <kevinngako@gmail.com>
2024-06-04 04:43:14 +0200

    Rebase et gestion des erreurs

--- www/Views/Security/profile.php
+++ www/Views/Security/profile.php
@@ -1,28 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-  <link rel="stylesheet" href="./css/navbar.css">
-</head>
-<body>
-  <!-- Navbar -->
-  <div class="navbar">
-    <a href="#">Accueil</a>
-    <a href="#">Profil</a>
-  
-    <?php
-    if (isset($pages) && !empty($pages)) {
-        foreach ($pages as $page) {
-            echo "<a href='/page/showPage?id={$page->getId()}'>{$page->getTitle()}</a>";
-        }
-    }
-    ?>
-    <a href="/security/logout">Déconnexion</a>
-  </div>
 
-  <!-- Contenu de la page -->
   <h1>Profile</h1>
   <p>Vous êtes connecté en tant que <?= htmlspecialchars($authUser->getLastname(), ENT_QUOTES, 'UTF-8') ?></p>
   <p>Prénom: <?= htmlspecialchars($authUser->getFirstname(), ENT_QUOTES, 'UTF-8') ?></p>
@@ -30,5 +6,5 @@
   <p>Email: <?= htmlspecialchars($authUser->getEmail(), ENT_QUOTES, 'UTF-8') ?></p>
   <p>session id: <?= htmlspecialchars($_SESSION['user_id'], ENT_QUOTES, 'UTF-8') ?></p>
 
-</body>
-</html>
+
+
