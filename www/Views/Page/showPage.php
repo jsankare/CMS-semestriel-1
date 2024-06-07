5acb47d13982c7f71052eec131632619611f482b
pierre265 <kevinngako@gmail.com>
2024-06-07 09:41:25 +0200

    Modif design pageComment

--- www/Views/Page/showPage.php
+++ www/Views/Page/showPage.php
@@ -1,14 +1,17 @@
-<section class="article--wrapper">
+<section class="comment--wrapper">
     <h2>Commentaire</h2>
+    <section class="comment--navigation">
+        <a href="/article/home"><img class="comment--icon" src="/assets/add.svg" alt="Mettre un commentaire" ></a>
+    </section>
     <ul>
-          <section class="article--wrapper__close">
-                <div class="article--wrapper__unit">
-                    <div class="article--infos">
-                        <li class="article--value">
+          <section class="comment--wrapper__close">
+                <div class="comment--wrapper__unit">
+                    <div class="comment--infos">
+                        <li class="comment--value">
                             <h3>Article</h3>
                             <p><?= htmlspecialchars($page->getTitle(), ENT_QUOTES, 'UTF-8') ?></p>
                         </li>
-                        <li class="article--value">
+                        <li class="comment--value">
                             <h3>Commentaire</h3>
                             <p><?= htmlspecialchars($page->getContent(), ENT_QUOTES, 'UTF-8') ?></p>
                         </li>
