80d0ea5005e3636b8c9ee77fb337cdd8814f094c
pierre265 <kevinngako@gmail.com>
2024-06-09 08:35:53 +0200

    Code for template page

--- www/Views/Page/showPage.php
+++ www/Views/Page/showPage.php
@@ -1,18 +1,19 @@
 <section class="comment--wrapper">
-    <h2>Commentaire</h2>
-    <section class="comment--navigation">
-        <a href="/article/home"><img class="comment--icon" src="/assets/add.svg" alt="Mettre un commentaire" ></a>
-    </section>
+    <h2>Page</h2>
     <ul>
           <section class="comment--wrapper__close">
                 <div class="comment--wrapper__unit">
                     <div class="comment--infos">
                         <li class="comment--value">
-                            <h3>Article</h3>
+                            <h3>Titre</h3>
                             <p><?= htmlspecialchars($page->getTitle(), ENT_QUOTES, 'UTF-8') ?></p>
                         </li>
                         <li class="comment--value">
-                            <h3>Commentaire</h3>
+                            <h3>Description</h3>
+                            <p><?= htmlspecialchars($page->getDescription(), ENT_QUOTES, 'UTF-8') ?></p>
+                        </li>
+                        <li class="comment--value">
+                            <h3>Contenu</h3>
                             <p><?= htmlspecialchars($page->getContent(), ENT_QUOTES, 'UTF-8') ?></p>
                         </li>
                     </div>
