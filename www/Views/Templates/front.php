605174603ff1c87df079ebd45c242bc999eb60ae
pierre265 <kevinngako@gmail.com>
2024-06-14 14:08:04 +0200

    dropdown menu

--- www/Views/Templates/front.php
+++ www/Views/Templates/front.php
@@ -23,7 +23,35 @@
     }
     ?>
     <body>
+<<<<<<< HEAD
         <?php include 'navbar.php'; ?>
+=======
+    <?php if (isset($_SESSION['user_id'])): ?>
+    
+    <div class="navbar">
+        <div class="navbar-logo">
+            <a href="index.php"><img src="logo.png" alt="CMS Logo"></a>
+        </div>
+        <a href="#">Accueil</a>
+        <a href="/profile">Profil</a>
+        <div class="dropdown">
+            <button class="dropbtn">Pages
+                <i class="fa fa-caret-down"></i>
+            </button>
+            <div class="dropdown-content">
+            <?php
+                if (isset($pages) && !empty($pages)) {
+                    foreach ($pages as $page) {
+                        echo "<a href='/page/showPage?id={$page->getId()}'>{$page->getTitle()}</a>";
+                    }
+                }
+            ?>
+            </div>
+        </div>
+        <a href="/logout" class="logout">Déconnexion</a>
+    </div>
+    <?php endif; ?>
+>>>>>>> e33db48 (dropdown menu)
         <h1>Template Front - CMS</h1>
 
         <?php include "../Views/".$this->view.".php";?>
