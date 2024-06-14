1657b93b68a2ee761ad3cc02c4c9be54a2f4b029
pierre265 <kevinngako@gmail.com>
2024-06-14 11:05:36 +0200

    use strips-tags for the content page

--- www/Views/Page/showPage.php
+++ www/Views/Page/showPage.php
@@ -1,20 +1,16 @@
-<section class="comment--wrapper">
+<section class="page--wrapper">
     <h2>Page</h2>
     <ul>
-          <section class="comment--wrapper__close">
-                <div class="comment--wrapper__unit">
-                    <div class="comment--infos">
-                        <li class="comment--value">
+          <section class="page--wrapper__close">
+                <div class="page--wrapper__unit">
+                    <div class="page--infos">
+                        <li class="page--value">
                             <h3>Titre</h3>
                             <p><?= htmlspecialchars($page->getTitle(), ENT_QUOTES, 'UTF-8') ?></p>
                         </li>
-                        <li class="comment--value">
-                            <h3>Description</h3>
-                            <p><?= htmlspecialchars($page->getDescription(), ENT_QUOTES, 'UTF-8') ?></p>
-                        </li>
-                        <li class="comment--value">
+                        <li class="page--value">
                             <h3>Contenu</h3>
-                            <p><?= htmlspecialchars($page->getContent(), ENT_QUOTES, 'UTF-8') ?></p>
+                            <p><?= strip_tags($page->getContent(), '<p><a><b><i><strong><em><ul><ol><li><br><h1><h2><h3><h4><h5><h6>') ?></p>
                         </li>
                     </div>
                 </div>
