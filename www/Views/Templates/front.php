3ddb79b2f5f64a692dfd7e6ea965a3602bd12365
pierre265 <kevinngako@gmail.com>
2024-06-18 21:25:54 +0200

    Front design

--- www/Views/Templates/front.php
+++ www/Views/Templates/front.php
@@ -23,31 +23,9 @@
     }
     ?>
     <body>
-    <div class="navbar" style="background-color: <?= $backgroundColor ?>; font-family: <?= $fontStyle ?>">
-        <ul>
-            <li><a style="color: <?= $fontColor ?>;" href="/">Accueil</a></li>
-            <li><a style="color: <?= $fontColor ?>;" href="/articles" >Articles</a></li>
-            <li><a style="color: <?= $fontColor ?>;" href="/gallery" >Galerie</a></li>
-        <?php
-        if (isset($pages) && !empty($pages)) {
-            foreach ($pages as $page) {
-                echo "<li><a style='color: " . $fontColor . ";' href='/page/showPage?id=" . $page->getId() . "'>" . $page->getTitle() . "</a></li>";
-            }
-        }
-        ?>
-        <?php if (isset($_SESSION['user_status']) && $_SESSION['user_status'] > 1) {
-            echo '<li><a href="/logout" class="logout" style="color: ' . $fontColor . '">Déconnexion</a></li>';
-            echo '<li><a href="/profile" class="logout" style="color: ' . $fontColor . '">Profil</a></li>';
-            echo '<li><a href="/dashboard" class="logout" style="color: ' . $fontColor . '">Dashboard</a></li>';
-        } else {
-            echo '<li><a href="/register" class="logout" style="color: ' . $fontColor . '">Inscription</a></li>';
-            echo '<li><a href="/login" class="logout" style="color: ' . $fontColor . '">Connexion</a></li>';
-        }
-        ?>
-        </ul>
-    </div>
-    <main style="font-family: <?= $fontStyle ?>">
-        <!-- intégration de la vue -->
+        <?php include 'navbar.php'; ?>
+        <h1>Template Front - CMS</h1>
+
         <?php include "../Views/".$this->view.".php";?>
     </main>
     </body>
