8d26810f0302588e64c0532e36f57a6bd044ea15
pierre265 <kevinngako@gmail.com>
2024-06-18 21:21:33 +0200

    navbar update

--- www/Views/Templates/navbar.php
+++ www/Views/Templates/navbar.php
@@ -1,29 +1,42 @@
+
 <div class="navbar">
     <div class="navbar-logo">
-        <a href="/"><img src="path/to/your/logo.png" alt="CMS Logo"></a>
+        <a href="index.php"><img src="path/to/your/logo.png" alt="CMS Logo"></a>
     </div>
-    
-    <?php if ($_SERVER['REQUEST_URI'] == '/login' || $_SERVER['REQUEST_URI'] == '/register'): ?>
-        <!-- Si l'utilisateur est sur la page de connexion ou de registre, ne rien afficher ici -->
-    
+    <?php if (!isset($_SESSION['user_id'])): ?>
+        <?php if ($_SERVER['REQUEST_URI'] == '/home'): ?>
+            
+            <?php
+            if (isset($pages) && !empty($pages)) {
+                foreach ($pages as $page) {
+                  echo "<a href='/page/{$page->getSlug()}/{$page->getId()}'>{$page->getTitle()}</a>";
+                }
+            }
+            ?>
+            <a href="/login">Se connecter</a>
+        <?php elseif ($_SERVER['REQUEST_URI'] == '/login'): ?>
+           
+        <?php else: ?>
+           
+            <a href="/home">Accueil</a>
+            <a href="/login">Se connecter</a>
+        <?php endif; ?>
     <?php else: ?>
+        <!-- Navbar for connected users -->
         <a href="/">Accueil</a>
+        <a href="/profile">Profil</a>
         <div class="dropdown">
             <button class="dropbtn">Pages</button>
             <div class="dropdown-content">
-                <?php if (!empty($pages)): ?>
-                    <?php foreach ($pages as $page): ?>
-                        <a href="/page/<?= htmlspecialchars($page->getSlug()) ?>"><?= htmlspecialchars($page->getTitle()) ?></a>
-                    <?php endforeach; ?>
-                <?php endif; ?>
+                <?php
+                if (isset($pages) && !empty($pages)) {
+                    foreach ($pages as $page) {
+                        echo "<a href='/page/{$page->getSlug()}/{$page->getId()}'>{$page->getTitle()}</a>";
+                    }
+                }
+                ?>
             </div>
         </div>
-        <?php if (!isset($_SESSION['user_id'])): ?>
-            <a href="/login" class="login">Se connecter</a>
-            <a href="/register" class="register">S'inscrire</a>
-        <?php else: ?>
-            <a href="/profile">Profil</a>
-            <a href="/logout" class="logout">Déconnexion</a>
-        <?php endif; ?>
+        <a href="/logout" class="logout">Déconnexion</a>
     <?php endif; ?>
 </div>
