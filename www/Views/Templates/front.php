b6d75dea07ff0aaa17fe97e272d6eb4179d218e0
pierre265 <kevinngako@gmail.com>
2024-06-18 21:25:54 +0200

    Front design

--- www/Views/Templates/front.php
+++ www/Views/Templates/front.php
@@ -10,27 +10,9 @@
 
     </head>
     <body>
-    <div class="navbar">
-        <a href="/">Accueil</a>
-        <a href="/articles" >Articles</a>
-        <a href="/gallery" >Galerie</a>
-        <?php
-        if (isset($pages) && !empty($pages)) {
-            foreach ($pages as $page) {
-                echo "<a href='/page/showPage?id={$page->getId()}'>{$page->getTitle()}</a>";
-            }
-        }
-        ?>
-        <?php if(isset($_SESSION['user_status']) && $_SESSION['user_status'] > 1) {
-            echo '<a href="/logout" class="logout">Déconnexion</a>';
-            echo '<a class="logout" href="/profile">Profil</a>';
-            echo '<a href="/dashboard" class="logout">Dashboard</a>';
-        } else {
-            echo '<a href="/register" class="logout">Inscription</a>';
-            echo '<a href="/login" class="logout">Connexion</a>';
-        }; ?>
-    </div>
-        <!-- intégration de la vue -->
+        <?php include 'navbar.php'; ?>
+        <h1>Template Front - CMS</h1>
+
         <?php include "../Views/".$this->view.".php";?>
     </body>
 </html>
