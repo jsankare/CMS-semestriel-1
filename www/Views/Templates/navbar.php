346a18ed691da1e09da84b3a40d5c2ba28d9b35c
pierre265 <kevinngako@gmail.com>
2024-06-25 10:21:58 +0200

    Template pour la navbar

--- www/Views/Templates/navbar.php
+++ www/Views/Templates/navbar.php
@@ -1,39 +1,37 @@
-
 <div class="navbar">
     <div class="navbar-logo">
-        <a href="index.php"><img src="path/to/your/logo.png" alt="CMS Logo"></a>
+        <a href="/"><img src="path/to/your/logo.png" alt="CMS Logo"></a>
     </div>
-    <?php if (!isset($_SESSION['user_id'])): ?>
-        <?php if ($_SERVER['REQUEST_URI'] == '/home'): ?>
-            
-            <?php
-            if (isset($pages) && !empty($pages)) {
-                foreach ($pages as $page) {
-                    echo "<a href='/page/{$page->getSlug()}/{$page->getId()}'>{$page->getTitle()}</a>";
-                }
-            }
-            ?>
-            <a href="/login">Se connecter</a>
-        <?php elseif ($_SERVER['REQUEST_URI'] == '/login'): ?>
-           
-        <?php else: ?>
-           
-            <a href="/home">Accueil</a>
-            <a href="/login" class="login">Se connecter</a> <a href="/register" class="register">S'inscrire</a>
-        <?php endif; ?>
+
+    <?php if ($_SERVER['REQUEST_URI'] == '/login'): ?>
+        <!-- Si l'utilisateur est sur la page de connexion, ne rien afficher ici -->
+    <?php elseif (!isset($_SESSION['user_id'])): ?>
+        <!-- Si l'utilisateur n'est pas connecté -->
+        <a href="/">Accueil</a>
+        <div class="dropdown">
+            <button class="dropbtn">Pages</button>
+            <div class="dropdown-content">
+                <?php if (!empty($pages)): ?>
+                    <?php foreach ($pages as $page): ?>
+                        <a href="/page/<?= $page->getSlug() ?>"><?= htmlspecialchars($page->getTitle()) ?></a>
+                    <?php endforeach; ?>
+                <?php endif; ?>
+            </div>
+        </div>
+        <a href="/login" class="login">Se connecter</a>
+        <a href="/register" class="register">S'inscrire</a>
     <?php else: ?>
+        <!-- Si l'utilisateur est connecté -->
         <a href="/">Accueil</a>
         <a href="/profile">Profil</a>
         <div class="dropdown">
             <button class="dropbtn">Pages</button>
             <div class="dropdown-content">
-                <?php
-                if (isset($pages) && !empty($pages)) {
-                    foreach ($pages as $page) {
-                        echo "<a href='/page/{$page->getSlug()}/{$page->getId()}'>{$page->getTitle()}</a>";
-                    }
-                }
-                ?>
+                <?php if (!empty($pages)): ?>
+                    <?php foreach ($pages as $page): ?>
+                        <a href="/page/<?= $page->getSlug() ?>"><?= htmlspecialchars($page->getTitle()) ?></a>
+                    <?php endforeach; ?>
+                <?php endif; ?>
             </div>
         </div>
         <a href="/logout" class="logout">Déconnexion</a>
