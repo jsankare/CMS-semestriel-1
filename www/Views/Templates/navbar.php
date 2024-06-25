e67a797a663b83bf3dea1946fb6882950b3c245a
pierre265 <kevinngako@gmail.com>
2024-06-25 19:48:14 +0200

    fonction sur la navbar

--- www/Views/Templates/navbar.php
+++ www/Views/Templates/navbar.php
@@ -2,26 +2,34 @@
     <div class="navbar-logo">
         <a href="/"><img src="path/to/your/logo.png" alt="CMS Logo"></a>
     </div>
-
-    <?php if ($_SERVER['REQUEST_URI'] == '/login'): ?>
-        <!-- Si l'utilisateur est sur la page de connexion, ne rien afficher ici -->
+    
+    <?php if ($_SERVER['REQUEST_URI'] == '/login' || $_SERVER['REQUEST_URI'] == '/register'): ?>
+        <!-- Si l'utilisateur est sur la page de connexion ou de registre, ne rien afficher ici -->
+    
     <?php elseif (!isset($_SESSION['user_id'])): ?>
-        <!-- Si l'utilisateur n'est pas connecté -->
-        <a href="/">Accueil</a>
-        <div class="dropdown">
-            <button class="dropbtn">Pages</button>
-            <div class="dropdown-content">
-                <?php if (!empty($pages)): ?>
-                    <?php foreach ($pages as $page): ?>
-                        <a href="/page/<?= $page->getSlug() ?>"><?= htmlspecialchars($page->getTitle()) ?></a>
-                    <?php endforeach; ?>
-                <?php endif; ?>
+        <?php if ($_SERVER['REQUEST_URI'] == '/home'): ?>
+            <?php if (!empty($pages)): ?>
+                <?php foreach ($pages as $page): ?>
+                    <a href="/page/<?= htmlspecialchars($page->getSlug()) ?>"><?= htmlspecialchars($page->getTitle()) ?></a>
+                <?php endforeach; ?>
+            <?php endif; ?>
+            <a href="/login">Se connecter</a>
+        <?php else: ?>
+            <a href="/">Accueil</a>
+            <div class="dropdown">
+                <button class="dropbtn">Pages</button>
+                <div class="dropdown-content">
+                    <?php if (!empty($pages)): ?>
+                        <?php foreach ($pages as $page): ?>
+                            <a href="/page/<?= htmlspecialchars($page->getSlug()) ?>"><?= htmlspecialchars($page->getTitle()) ?></a>
+                        <?php endforeach; ?>
+                    <?php endif; ?>
+                </div>
             </div>
-        </div>
-        <a href="/login" class="login">Se connecter</a>
-        <a href="/register" class="register">S'inscrire</a>
+            <a href="/login" class="login">Se connecter</a>
+            <a href="/register" class="register">S'inscrire</a>
+        <?php endif; ?>
     <?php else: ?>
-        <!-- Si l'utilisateur est connecté -->
         <a href="/">Accueil</a>
         <a href="/profile">Profil</a>
         <div class="dropdown">
@@ -29,7 +37,7 @@
             <div class="dropdown-content">
                 <?php if (!empty($pages)): ?>
                     <?php foreach ($pages as $page): ?>
-                        <a href="/page/<?= $page->getSlug() ?>"><?= htmlspecialchars($page->getTitle()) ?></a>
+                        <a href="/page/<?= htmlspecialchars($page->getSlug()) ?>"><?= htmlspecialchars($page->getTitle()) ?></a>
                     <?php endforeach; ?>
                 <?php endif; ?>
             </div>
