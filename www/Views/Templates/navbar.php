f31e7817c3578634dbc35ad22056ce9c6322a0ec
pierre265 <kevinngako@gmail.com>
2024-06-25 20:00:17 +0200

    Navbar for page

--- www/Views/Templates/navbar.php
+++ www/Views/Templates/navbar.php
@@ -6,32 +6,8 @@
     <?php if ($_SERVER['REQUEST_URI'] == '/login' || $_SERVER['REQUEST_URI'] == '/register'): ?>
         <!-- Si l'utilisateur est sur la page de connexion ou de registre, ne rien afficher ici -->
     
-    <?php elseif (!isset($_SESSION['user_id'])): ?>
-        <?php if ($_SERVER['REQUEST_URI'] == '/home'): ?>
-            <?php if (!empty($pages)): ?>
-                <?php foreach ($pages as $page): ?>
-                    <a href="/page/<?= htmlspecialchars($page->getSlug()) ?>"><?= htmlspecialchars($page->getTitle()) ?></a>
-                <?php endforeach; ?>
-            <?php endif; ?>
-            <a href="/login">Se connecter</a>
-        <?php else: ?>
-            <a href="/">Accueil</a>
-            <div class="dropdown">
-                <button class="dropbtn">Pages</button>
-                <div class="dropdown-content">
-                    <?php if (!empty($pages)): ?>
-                        <?php foreach ($pages as $page): ?>
-                            <a href="/page/<?= htmlspecialchars($page->getSlug()) ?>"><?= htmlspecialchars($page->getTitle()) ?></a>
-                        <?php endforeach; ?>
-                    <?php endif; ?>
-                </div>
-            </div>
-            <a href="/login" class="login">Se connecter</a>
-            <a href="/register" class="register">S'inscrire</a>
-        <?php endif; ?>
     <?php else: ?>
         <a href="/">Accueil</a>
-        <a href="/profile">Profil</a>
         <div class="dropdown">
             <button class="dropbtn">Pages</button>
             <div class="dropdown-content">
@@ -42,6 +18,12 @@
                 <?php endif; ?>
             </div>
         </div>
-        <a href="/logout" class="logout">Déconnexion</a>
+        <?php if (!isset($_SESSION['user_id'])): ?>
+            <a href="/login" class="login">Se connecter</a>
+            <a href="/register" class="register">S'inscrire</a>
+        <?php else: ?>
+            <a href="/profile">Profil</a>
+            <a href="/logout" class="logout">Déconnexion</a>
+        <?php endif; ?>
     <?php endif; ?>
 </div>
